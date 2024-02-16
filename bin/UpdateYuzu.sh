ca66553eb0ca5e245dfafd28513e53fd2661e56c
Jay Graves <jay@skabber.com>
2024-02-16 08:55:21 -0700

    start some bin scripts

---
+++ bin/UpdateYuzu.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+curl -s https://api.github.com/repos/pineappleEA/pineapple-src/releases/latest | jq -r ".assets[0] | .browser_download_url" | wget -qO ~/Applications/yuzu.AppImage -i -
