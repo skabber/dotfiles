2a36176eaeb6119cb7c3fb1057fbd5b538d3f771
Jay Graves <jay@skabber.com>
2026-04-07 07:35:44 -0600

    ollama fix

--- modules/services/ollama.nix
+++ modules/services/ollama.nix
@@ -32,7 +32,7 @@ in
 
     services.ollama = {
         enable = true;
-        # package = pkgs.ollama-rocm;
+        package = pkgs.ollama-rocm;
         environmentVariables = {
             HCC_AMDGPU_TARGET = "gfx1030";
             HSA_OVERRIDE_GFX_VERSION = "10.3.0";
