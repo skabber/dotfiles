f6dd59fb5c351c68e5b5c966b961c5a0f6d2ae17
Jay Graves <jay@skabber.com>
2026-07-15 19:15:30 -0600

    chore: bump zcode to 3.3.6
    Update download URL pattern to new CDN layout
    (releases/<version>/linux-x64/...)

--- pkgs/zcode.nix
+++ pkgs/zcode.nix
@@ -18,10 +18,10 @@
   copyDesktopItems,
 }:
 let
-  version = "3.3.5";
+  version = "3.3.6";
   src = fetchurl {
-    url = "https://cdn-zcode.z.ai/zcode/electron/releases/${version}/ZCode-${version}-linux-x64.AppImage";
-    hash = "sha256-27QKoGN0ntaGlatXRn8Rq0vFlxeBnmyCkgqNSy6yISg=";
+    url = "https://cdn-zcode.z.ai/zcode/electron/releases/${version}/linux-x64/ZCode-${version}-linux-x64.AppImage";
+    hash = "sha256-496GvfbpN8PCqXuAlMVkyvbRsQ1GmUIBHhcegzRaP6E=";
   };
 
   bin = writeShellScriptBin "zcode" ''
