4438e3d07467f5e51c562e15e3e0fc57a0087642
Jay Graves <jay@skabber.com>
2026-09-01 10:14:16 -0600

    zcode update

--- pkgs/zcode.nix
+++ pkgs/zcode.nix
@@ -18,10 +18,10 @@
   copyDesktopItems,
 }:
 let
-  version = "3.10.1";
+  version = "3.10.2";
   src = fetchurl {
     url = "https://cdn-zcode.z.ai/zcode/electron/releases/${version}/linux-x64/ZCode-${version}-linux-x64.AppImage";
-    hash = "sha256-9T/d0uf3roTimyZgy0oMaHyUmdZbmKdOIifAAWdOI1o=";
+    hash = "sha256-b0utaKoaaQJuikXQqd8l8YaDvJpBevSDEF3L70SLqz8=";
   };
 
   bin = writeShellScriptBin "zcode" ''
