c6f8320ea4a4814b5f427010274117dcfbcc52f8
Jay Graves <jay@skabber.com>
2026-09-20 11:00:24 -0600

    chore: update zcode to 3.14.0

--- pkgs/zcode.nix
+++ pkgs/zcode.nix
@@ -18,10 +18,10 @@
   copyDesktopItems,
 }:
 let
-  version = "3.11.2";
+  version = "3.14.0";
   src = fetchurl {
     url = "https://cdn-zcode.z.ai/zcode/electron/releases/${version}/linux-x64/ZCode-${version}-linux-x64.AppImage";
-    hash = "sha256-/EzIUShqQOqAkM6/qsGp+b3ETqs0njbu8NOzIZ85MD8=";
+    hash = "sha256-GBKweecQjp6bVF+OHxPguJ6ndfmE/mUH77eav5P1fHk=";
   };
 
   bin = writeShellScriptBin "zcode" ''
